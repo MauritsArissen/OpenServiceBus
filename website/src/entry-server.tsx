9eed6846cc9374479a84abdf4901785dcb715e3a
MauritsArissen <marissen1@live.nl>
2026-08-24 12:23:03 +0200

    perf: code-split the examples page out of the homepage bundle

--- website/src/entry-server.tsx
+++ website/src/entry-server.tsx
@@ -1,11 +1,21 @@
 import { renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router";
-import { AppRoutes } from "./App";
+import { Routes, Route } from "react-router-dom";
+import { PageShell } from "./App";
+import HomePage from "./pages/HomePage";
+import ExamplesPage from "./pages/ExamplesPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 export function render(url: string): string {
   return renderToString(
     <StaticRouter location={url}>
-      <AppRoutes />
+      <PageShell>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/examples" element={<ExamplesPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </PageShell>
     </StaticRouter>,
   );
 }
